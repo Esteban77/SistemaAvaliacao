05ba4e06d80c060e328feb82bcad0531e4948832
Esteban.Bustamante <esteban.tec7@gmail.com>
2016-11-19 15:32:20 -0200

    comit

--- WebContent/js/ajax.js
+++ WebContent/js/ajax.js
@@ -120,36 +120,7 @@ $(document).ready(function() {
 	});		
 
 	
-		$('#enviar').click(function(){
-			var respostas = new Array();
-			var idPergunta1;
-			var comentario= $('#comentario').val();
-			
-			$('input').each(function(){
-				if($(this).attr("name")=="idPergunta"){
-//					alert($(this).attr("name"));
-					idPergunta1 = $(this).val();
-				}				
-				if($(this).is(':checked')){
-//					alert("true");
-					respostas.push({
-						idPergunta: idPergunta1,
-						resposta: $(this).val()
-					});
-				}
-				
-			});
-					$.getJSON('/SistemaAvaliacao/FrontController?acao=SalvarRespostasFormulario',	{'respostas': JSON.stringify(respostas), 'comentario': comentario}, 		
-							//Funcao de callback
-							function(responseTxt, statusTxt, xhr) { 
-						if(statusTxt == "success"){
-							var repostas = responseTxt;
-						}if(statusTxt == "error"){
-//							alert("Error: " + xhr.status + ": " + xhr.statusText);
-						}
-							});
-				
-			});
+
 		
 });
 
--- WebContent/js/funcoesFormularioCliente.js
+++ WebContent/js/funcoesFormularioCliente.js
@@ -3,4 +3,35 @@ $(document).ready(function(){
         $("#modalConsumidor").modal();
         $('confrimar').attr("data-dismiss", "modal");
     });
+	
+	$('#enviar').click(function(){
+		var respostas = new Array();
+		var idPergunta1;
+		var comentario= $('#comentario').val();
+		
+		$('input').each(function(){
+			if($(this).attr("name")=="idPergunta"){
+//				alert($(this).attr("name"));
+				idPergunta1 = $(this).val();
+			}				
+			if($(this).is(':checked')){
+//				alert("true");
+				respostas.push({
+					idPergunta: idPergunta1,
+					resposta: $(this).val()
+				});
+			}
+			
+		});
+				$.getJSON('/SistemaAvaliacao/FrontController?acao=SalvarRespostasFormulario',	{'respostas': JSON.stringify(respostas), 'comentario': comentario}, 		
+						//Funcao de callback
+						function(responseTxt, statusTxt, xhr) { 
+					if(statusTxt == "success"){
+						var repostas = responseTxt;
+					}if(statusTxt == "error"){
+//						alert("Error: " + xhr.status + ": " + xhr.statusText);
+					}
+						});
+			
+		});
 }); 
